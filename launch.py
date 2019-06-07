b423b5c0f394032ac032885d194af8645defc31a
veerkoti <30378185+veerkoti@users.noreply.github.com>
2019-06-08 02:47:01 +0530

    path should be changed before execution
    path should be changed before execution

--- launch.py
+++ launch.py
@@ -1,6 +1,7 @@
 import subprocess
 import time
 from appium import webdriver
+nodepath = 'Change this to your appium server folder'
 
 class install_and_launch:
     def __init__(self):
@@ -14,13 +15,14 @@ class install_and_launch:
             'platformVersion':'9'
         }
     def install_and_launch(self,app):
+        global nodepath
         self.dc['app']=app
-        server = subprocess.Popen(["D:/Downloads/Appium/appium_server/node.exe","D:/Downloads/Appium/appium_server/appium/build/lib/main.js"])
+        server = subprocess.Popen([nodepath+"/node.exe",nodepath+"/appium/build/lib/main.js"])
         time.sleep(5)
         self.driver = webdriver.Remote(self.url,self.dc)
 
 launch_obj = install_and_launch()
-launch_obj.install_and_launch('D:/Downloads/Appium/appium_server/appium/node_modules/appium-uiautomator2-server/app/src/androidTestE2eTest/java/io/appium/uiautomator2/unittest/resource/ApiDemos-debug.apk')
+launch_obj.install_and_launch(nodepath+'/appium/node_modules/appium-uiautomator2-server/app/src/androidTestE2eTest/java/io/appium/uiautomator2/unittest/resource/ApiDemos-debug.apk')
 text_objects = launch_obj.driver.find_elements_by_class_name('android.widget.TextView')
 # textbox_objects = launch_obj.driver.find_elements_by_class_name('android.widget.EditText')
 print('text_objects',text_objects)
